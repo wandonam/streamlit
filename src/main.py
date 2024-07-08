ca48f4bafcca5e820cffc0da45425d77e0c9c5fb
wandonam <sinholy91@gmail.com>
2024-07-08 17:14:16 +0900

    triz_gross.ver1

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ if menu == 'Total Sales':
     sales_per_date = filtered_data.groupby('date')['sales'].sum().reset_index()
 
     # 날짜 형식을 "MM-DD"로 변경
-    sales_per_date['formatted_date'] = sales_per_date['date'].dt.strftime('%m-%d')
+    sales_per_date['formatted_date'] = sales_per_date['date'].dt.strftime('%m-%d (%a)')
 
     # 그래프 유형 선택 드롭다운 메뉴
     graph_type = st.selectbox('Select Graph Type', ['Line Plot', 'Bar Plot'])
@@ -101,7 +101,7 @@ if menu == 'Total Sales':
         compare_sales_per_date.index = sales_per_date.index
 
         # 비교 기간의 날짜 형식을 "MM-DD"로 변경
-        compare_sales_per_date['formatted_date'] = compare_sales_per_date['date'].dt.strftime('%m-%d')
+        compare_sales_per_date['formatted_date'] = compare_sales_per_date['date'].dt.strftime('%m-%d (%a)')
 
         # 비교 기간 매출 그래프 추가
         if graph_type == 'Line Plot':
@@ -116,7 +116,6 @@ if menu == 'Total Sales':
     plt.legend()
     st.pyplot(plt)
 
-
 elif menu == 'Trend by Channel':
     st.write('#### **Trend by Channel**')
     st.write('##### Dataframe')
@@ -126,15 +125,24 @@ elif menu == 'Trend by Channel':
     channel_pivot = channel_pivot.sort_index(ascending=False)
     channel_pivot = channel_pivot.fillna(0)
     channel_pivot = channel_pivot.astype(int)
-    channel_pivot.index = pd.to_datetime(channel_pivot.index).strftime('%y-%m-%d')
+    channel_pivot.index = pd.to_datetime(channel_pivot.index).strftime('%y-%m-%d (%a)')
 
-    gb = GridOptionsBuilder.from_dataframe(channel_pivot.reset_index())
+    def format_number(value):
+        return f"{value:,}"
+
+    format_pivot = channel_pivot.applymap(format_number)
+
+    gb = GridOptionsBuilder.from_dataframe(format_pivot.reset_index())
     gb.configure_default_column(filterable=True, sortable=True)
     gb.configure_pagination(paginationAutoPageSize=True)
     gridOptions = gb.build()
 
-    # AgGrid를 사용해 데이터프레임 표시
-    aggrid_response = AgGrid(channel_pivot.reset_index(), gridOptions=gridOptions, enable_enterprise_modules=True)
+    aggrid_response = AgGrid(
+        format_pivot.reset_index(),
+        gridOptions=gridOptions,
+        enable_enterprise_modules=True,
+        fit_columns_on_grid_load=True,
+        update_mode='MODEL_CHANGED')
 
 
     st.write('##### Vizualization')
@@ -158,7 +166,7 @@ elif menu == 'Trend by Channel':
     sales_per_date_channel = filtered_data.groupby(['date', 'channel'])['sales'].sum().unstack().fillna(0)
 
     # 날짜 형식을 "MM-DD"로 변경
-    sales_per_date_channel.index = sales_per_date_channel.index.strftime('%m-%d')
+    sales_per_date_channel.index = sales_per_date_channel.index.strftime('%m-%d (%a)')
 
     # 매출 그래프 그리기
     plt.figure(figsize=(14, 8))
@@ -215,4 +223,133 @@ elif menu == 'Trend by Channel':
                     plt.text(i, sales, f'{sales/1e6:.1f}M', fontsize=8, ha='center', va='bottom')
 
     plt.legend()
+    st.pyplot(plt)
+
+elif menu == 'Trend by Product':
+    st.write('#### **Trend by Product**')
+    st.write('##### Product')
+
+    if 'selected_products' not in st.session_state:
+        st.session_state.selected_products = list(df_gross['product'].unique())
+
+    # 초기 세팅: 마지막 일의 해당 월의 1일
+    initial_end_date = df_gross['date'].max()
+    initial_start_date = initial_end_date.replace(day=1)
+
+    date_range = st.date_input('SELECT DATE', (initial_start_date, initial_end_date))
+    start_date, end_date = date_range
+
+    # 상품 목록
+    products = df_gross['product'].unique()
+
+    # 선택한 날짜 범위 및 제품에 따라 데이터 필터링
+    filtered_data = df_gross[(df_gross['date'] >= pd.to_datetime(start_date)) & 
+                             (df_gross['date'] <= pd.to_datetime(end_date)) & 
+                             (df_gross['product'].isin(st.session_state.selected_products))]
+
+    # 날짜 및 제품별 매출 합계 계산
+    total_sales_per_product = filtered_data.groupby('product')['sales'].sum()
+
+    # 기간 내 일수 계산
+    num_days = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days + 1
+    
+    average_daily_sales_per_product = total_sales_per_product / num_days
+    filtered_products = average_daily_sales_per_product[average_daily_sales_per_product > 100000].index
+
+
+    sales_per_product = total_sales_per_product[filtered_products]
+    sales_per_product = sales_per_product.sort_values(ascending=False)
+
+    selected_products = [p for p in st.session_state.selected_products if p in sales_per_product.index]
+    updated_selected_products = st.multiselect('SELECT PRODUCT', products, default=selected_products)
+    st.session_state.selected_products = updated_selected_products
+
+    # 필터링된 데이터를 다시 계산
+    filtered_data = df_gross[(df_gross['date'] >= pd.to_datetime(start_date)) & 
+                             (df_gross['date'] <= pd.to_datetime(end_date)) & 
+                             (df_gross['product'].isin(st.session_state.selected_products))]
+
+    # 다시 계산된 데이터를 사용하여 매출 합계 재계산
+    sales_per_product = filtered_data.groupby('product')['sales'].sum()
+    sales_per_product = sales_per_product[average_daily_sales_per_product[average_daily_sales_per_product > 100000].index]
+    sales_per_product = sales_per_product.sort_values(ascending=False)
+
+    plt.figure(figsize=(14, 8))
+    bars = sales_per_product.plot(kind='bar', color=plt.cm.Paired(range(len(sales_per_product))))
+
+    plt.xlabel('Product')
+    plt.ylabel('Sales')
+    plt.title('Sales by Product')
+    plt.grid(True)
+
+    plt.xticks(rotation=45)
+
+    for i, sales in enumerate(sales_per_product):
+        plt.text(i, sales, f'{sales/1e6:.1f}M', fontsize=12, ha='center', va='bottom')
+    
+    st.pyplot(plt)
+
+    st.write('##### Option')
+    selected_product = st.selectbox('SELECT PRODUCT', sales_per_product.index)
+    if selected_product:
+        product_data = filtered_data[filtered_data['product'] == selected_product]
+        option_distribution = product_data['option'].value_counts(normalize=True) * 100
+
+    if selected_product:
+        product_data = filtered_data[filtered_data['product'] == selected_product]
+        option_sales_distribution = product_data.groupby('option')['sales'].sum()
+        option_sales_percentage = (option_sales_distribution / option_sales_distribution.sum()) * 100
+
+    plt.figure(figsize=(14, 14))
+    plt.pie(option_distribution, labels=option_distribution.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired(range(len(option_distribution))))
+    plt.title(f'Sales by Option: {selected_product}')
+    st.pyplot(plt)
+
+elif menu == 'Detailed by Channel':
+    st.write('#### **Detailed by Channel**')
+
+    # 초기 세팅: 마지막 일의 해당 월의 1일
+    initial_end_date = df_gross['date'].max()
+    initial_start_date = initial_end_date.replace(day=1)
+
+    date_range = st.date_input('SELECT DATE', (initial_start_date, initial_end_date))
+    start_date, end_date = date_range
+
+    channels = df_gross['channel'].unique()
+    selected_channels = st.selectbox('SELECT CHANNEL', channels, index=0)
+
+    # 선택한 날짜 범위 및 채널에 따라 데이터 필터링
+    filtered_data = df_gross[(df_gross['date'] >= pd.to_datetime(start_date)) &
+                             (df_gross['date'] <= pd.to_datetime(end_date)) &
+                             (df_gross['channel'] == selected_channels)]
+
+    # 날짜 및 제품별 매출 합계 계산
+    total_sales_per_product = filtered_data.groupby('product')['sales'].sum()
+
+    # 기간 내 일수 계산
+    num_days = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days + 1
+    
+    # 평균 일일 매출 계산
+    average_daily_sales_per_product = total_sales_per_product / num_days
+
+    # 평균 일일 매출이 50,000 이상인 제품 필터링
+    filtered_products = average_daily_sales_per_product[average_daily_sales_per_product > 50000].index
+
+    # 총 매출이 50,000 이상인 제품 필터링
+    sales_per_product = total_sales_per_product.loc[filtered_products]
+    sales_per_product = sales_per_product.sort_values(ascending=False)
+
+    plt.figure(figsize=(14, 8))
+    bars = sales_per_product.plot(kind='bar', color=plt.cm.Paired(range(len(sales_per_product))))
+    
+    plt.xlabel('Product')
+    plt.ylabel('Total Sales')
+    plt.title(f'Detailed by {selected_channels}')
+    plt.grid(True)
+
+    plt.xticks(rotation=45)
+    
+    for i, sales in enumerate(sales_per_product):
+        plt.text(i, sales, f'{sales/1e6:.1f}M', fontsize=12, ha='center', va='bottom')
+    
     st.pyplot(plt)
